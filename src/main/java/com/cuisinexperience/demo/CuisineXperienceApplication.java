f951908fe7a69ec3b15e6c509abbc02988f940e4
Joshua-Talltree <joshua.scott.talltree@gmail.com>
2021-04-26 17:07:06 -0500

    feat: all files that did not make it in the first push.

---
+++ src/main/java/com/cuisinexperience/demo/CuisineXperienceApplication.java
@@ -0,0 +1,13 @@
+package com.cuisinexperience.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuisineXperienceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CuisineXperienceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cuisinexperience/demo/CuisineXperienceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cuisinexperience.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuisineXperienceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
