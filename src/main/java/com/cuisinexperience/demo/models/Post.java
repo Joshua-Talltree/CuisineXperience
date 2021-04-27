32be69449d52bd13be9d406649c75928c07601a6
Joshua-Talltree <joshua.scott.talltree@gmail.com>
2021-04-27 13:43:33 -0500

    feat: created user and post models to begin the database construction.

---
+++ src/main/java/com/cuisinexperience/demo/models/Post.java
@@ -0,0 +1,79 @@
+package com.cuisinexperience.demo.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "posts")
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title", length = 225, nullable = false)
+    private String title;
+
+    @Column(name = "content", columnDefinition = "TEXT", length = 3000, nullable = false)
+    private String content;
+
+    @Column(name = "time_posted")
+    private String timePosted;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @ManyToOne()
+    @JoinColumn(name = "user_id")
+    private User owner;
+
+    public Post() {
+    }
+
+    public Post(String title, String content, String timePosted, String imageUrl, User owner) {
+        this.title = title;
+        this.content = content;
+        this.timePosted = timePosted;
+        this.imageUrl = imageUrl;
+        this.owner = owner;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getTimePosted() {
+        return timePosted;
+    }
+
+    public void setTimePosted(String timePosted) {
+        this.timePosted = timePosted;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
