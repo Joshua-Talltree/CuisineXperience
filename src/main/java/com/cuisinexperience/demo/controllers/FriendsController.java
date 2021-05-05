8c0513773cfdfa249d42988970b837d7a50ece30
Joshua-Talltree <joshua.scott.talltree@gmail.com>
2021-05-05 16:09:27 -0500

    feat: created an image gallery for each user to showcase their images.

---
+++ src/main/java/com/cuisinexperience/demo/controllers/FriendsController.java
@@ -0,0 +1,43 @@
+package com.cuisinexperience.demo.controllers;
+
+import com.cuisinexperience.demo.models.Friends;
+import com.cuisinexperience.demo.models.User;
+import com.cuisinexperience.demo.repos.FriendsRepository;
+import com.cuisinexperience.demo.repos.UserRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class FriendsController {
+
+    private final FriendsRepository friendsDao;
+    private final UserRepository userDao;
+
+    public FriendsController(FriendsRepository friendsDao, UserRepository userDao) {
+        this.friendsDao = friendsDao;
+        this.userDao = userDao;
+    }
+
+    @GetMapping("/user/friends")
+    public String viewFriends(Model vModel, Long id) {
+        User loggedInUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        List<Friends> friendsList = friendsDao.getFriendsById(id);
+        List<User> userList = new ArrayList<User>();
+        for (Friends friend : friendsList) {
+            if (userDao.findUserById(friend.getUserRecipientId().getId()).getId().equals(loggedInUser.getId())) {
+                userList.add(userDao.findUserById(friend.getUserSenderId().getId()));
+            } else {
+                userList.add(userDao.findUserById(friend.getUserRecipientId().getId()));
+            }
+        }
+        vModel.addAttribute("friends", userList);
+        vModel.addAttribute("owner", loggedInUser);
+        return "profile";
+    }
+
+}
