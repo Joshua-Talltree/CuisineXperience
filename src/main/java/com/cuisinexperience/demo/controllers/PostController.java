a2b403720c456e44dd5ddb0c98423100025cd27c
Joshua-Talltree <joshua.scott.talltree@gmail.com>
2021-05-21 12:30:30 -0500

    feat: hid the update and delete buttons from users that do not own the posts, fix the categories page to display the post within the categories, and ordered the posts from recent to oldest.

--- src/main/java/com/cuisinexperience/demo/controllers/PostController.java
+++ src/main/java/com/cuisinexperience/demo/controllers/PostController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @Controller
@@ -32,23 +33,26 @@ public class PostController {
     @GetMapping("/posts")
     public String allPosts(Model vModel) {
         List<Post> postsFromDB = postDao.findAll();
+        Collections.sort(postsFromDB, Collections.reverseOrder());
         vModel.addAttribute("posts", postsFromDB);
         return "/index";
     }
 
     @GetMapping("/posts/{id}")
     public String individualPosts(@PathVariable Long id, String name, Model vModel) {
-//        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-//        User loggedInUser = userDao.getOne(user.getId());
-//        List<User> users = userDao.findAll();
-//        vModel.addAttribute("users", users);
-        vModel.addAttribute("post", postDao.getOne(id));
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User loggedInUser = userDao.getOne(user.getId());
+        Post posts = postDao.getOne(id);
+        vModel.addAttribute("post", posts);
 
-//        boolean isPostOwner = false;
-//
-//        if (loggedInUser.size() > 0) {
-//            return "/show";
-//        }
+        boolean isOwner = false;
+
+        if(posts.getOwner().equals(loggedInUser)) {
+            isOwner = true;
+        }
+
+        vModel.addAttribute("isOwner", isOwner);
+
         return "/show";
     }
 
@@ -162,5 +166,15 @@ public class PostController {
         userDao.save(userToAdd);
 
         return "redirect:/posts";
+    }
+
+    @GetMapping("profile/category/{categoryId}")
+    public String postsInCategories(@PathVariable Long categoryId, Model vModel) {
+        List<Post> posts = categoriesDao.getOne(categoryId).getPosts();
+        Categories category = categoriesDao.getOne(categoryId);
+        vModel.addAttribute("posts", posts);
+        vModel.addAttribute("category", category);
+
+        return "/post-categories";
     }
 }
