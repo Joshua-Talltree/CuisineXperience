1ac1a60930cc3f9612d8676ea810a1dc68c3d0b9
Joshua-Talltree <joshua.scott.talltree@gmail.com>
2021-07-19 16:25:23 -0500

    feat: added a navbar and tailwind css to frontend project.

---
+++ cuisinexperience-frontend/src/components/NavBar.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { NavLink } from "react-router-dom";
+import { SocialIcon } from "react-social-icons";
+
+export default function NavBar() {
+    return (
+        <header className="bg-blue-600">
+            <div className="container mx-auto flex justify-between">
+                <nav className="flex">
+                    <NavLink
+                        to="/"
+                        exact
+                        activeClassName="text-white"
+                        className="inflex-flex items-center py-6 px-3 mr-4 text-blue-100 hover:text-green-800 text-4xl font-bold tracking-widest"
+                    >
+                        Joshua Talltree
+                    </NavLink>
+                    <NavLink
+                        to="/post"
+                        className="inline-flex py-3 px-3 my-6 rounded text-blue-200 hover:text-green-800"
+                        activeClassName="text-blue-100 bg-blue-700"
+                    >
+                        Blog Posts
+                    </NavLink>
+                    <NavLink
+                        to="/project"
+                        className="inline-flex py-3 px-3 my-6 rounded text-blue-200 hover:text-green-800"
+                        activeClassName="text-blue-100 bg-blue-700"
+                    >
+                        Projects
+                    </NavLink>
+                    <NavLink
+                        to="/about"
+                        className="inline-flex py-3 px-3 my-6 rounded text-blue-200 hover:text-green-800"
+                        activeClassName="text-blue-100 bg-blue-700"
+                    >
+                        About Me!
+                    </NavLink>
+                </nav>
+                <div className="inline-flex py-3 px-3 my-6">
+                    <SocialIcon
+                        url="https://www.linkedin.com/in/joshua-talltree/"
+                        className="mr-4"
+                        target="_blank"
+                        fgColor="#fff"
+                        style={{ height: 35, width: 35 }}
+                    />
+                    <SocialIcon
+                        url="https://www.reddit.com/user/Good_Historian3282"
+                        className="mr-4"
+                        target="_blank"
+                        fgColor="#fff"
+                        style={{ height: 35, width: 35 }}
+                    />
+                </div>
+            </div>
+        </header>
+    );
+}
